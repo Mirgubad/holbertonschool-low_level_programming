17bf071624f1fe4d178ad7bb3a978799f9a117ce
Mirgubad <mirgubadma@code.edu.az>
2024-02-19 17:22:55 +0000

    lasat digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,18 +11,18 @@ int main(void)
         n = rand() - RAND_MAX / 2;
         /* your code goes there */
 
-        int lastDigit=n%10;
+        
 
-        if(lastDigit>5){
+        if(n>5){
 
-                printf("and is greater than 5")
+                printf("%d is greater than 5",n)
         }
         else if(lastDigit==0){
 
-                printf("and is 0")
+                printf("%d is 0",n)
         }
-        else if(lastDigit<6 && lastDigit!=0){
-        printf("and is less than 6 and not 0")
+        else {
+        printf("%d is less than 6 and not 0",n)
         }
         return (0);
 }
