4831d75aa4815fd9595ad28eccc2b610303ffe30
Mirgubad <mirgubadma@code.edu.az>
2024-02-19 16:20:05 +0000

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include stdio <stdio.h>
+#include <stdio.h>
 
-
-int main(void){
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-
