1361db5c12d51c6920ac0194386aa861bdc467ee
Mirgubad <Mirgubadma@code.edu.az>
2024-02-20 08:49:24 +0000

    functions

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printText(void)
+	{
+		printf("__putchar");
+		printf("\n");
+	}
+
+	printText();
+	return (0);
+}
