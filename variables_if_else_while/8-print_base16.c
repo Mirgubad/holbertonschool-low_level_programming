d1d40b1336c94ae4a87d27ee0d77728ff3cad4d3
Mirgubad <Mirgubadma@code.edu.az>
2024-02-20 07:55:22 +0000

    printbase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch;
+	int num;
+
+	for (num = 0;num < 10; num++)
+		putchar(num + '0');
+
+	for (ch = 'a';ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
