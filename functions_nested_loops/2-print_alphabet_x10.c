3c3e35d46ef61186e7c8952021fe53505171aa49
Mirgubad <Mirgubadma@code.edu.az>
2024-02-20 14:01:11 +0000

    print alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,12 @@
 void print_alphabet_x10(void)
 {
 	int n, co;
-
+	char word[] = "abcdefghijklmnopqrstuvwxyz";
 	co = 0;
 
 	while (co < 10)
 	{
-		for (n = 'a'; n <= 'z'; n++)
-		{
-			_putchar(n);
-		}
+		_putchar(word);
 		co++;
 		_putchar('\n');
 	}
