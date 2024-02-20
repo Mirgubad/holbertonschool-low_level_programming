7c7dcae5b61d1784436392f3940e714b6fbf3506
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2024-02-20 00:32:32 -0800

    Update 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-   char ch;
-   
-   for(ch = 'a'; ch <= 'z'; ch++)
-      putchar(ch);
+	char letter;
 
-   putchar('\n');
-   return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
