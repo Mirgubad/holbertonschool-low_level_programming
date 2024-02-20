aca9a4407adb62bc828a32bff631a9dab0533da5
Mirgubad <Mirgubadma@code.edu.az>
2024-02-20 13:59:40 +0000

    print alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * main - Prints "alphabet" 10x
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
- * Return: ""
+ * Return: x10 a-z
  */
-
 void print_alphabet_x10(void)
 {
 	int n, co;
